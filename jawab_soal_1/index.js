90795cbd238c631907d126f019a45e346c2532ac
traxdize <kenneth.a.jims@gmail.com>
2024-03-05 23:55:15 +0700

    Contoh penggunaan Axios sebagai fungsi simple "GET"

--- jawab_soal_1/index.js
+++ jawab_soal_1/index.js
@@ -1,1 +1,13 @@
 const axios = require("axios")
+
+axios.get('/user?ID=12345')
+    .then(function(response) {
+        console.log(response)
+    })
+
+    .catch(function(error) {
+        console.log(error)
+    })
+    .finally(function(){
+
+    });
